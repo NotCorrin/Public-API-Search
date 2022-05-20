dc6582fee0cf4b5b77251cc46c58edef44d4c510
NotCorrin <43767802+NotCorrin@users.noreply.github.com>
2022-05-20 22:39:48 +1000

    Animated Drop Down

--- Public-API-Search/SearchViewController.swift
+++ Public-API-Search/SearchViewController.swift
@@ -9,19 +9,26 @@ import UIKit
 
 class SearchViewController: UIViewController {
     @IBOutlet weak var categoriesScrollView: UIScrollView!
+    @IBOutlet weak var categoriesButton: UIButton!
+    
     let numberOfButtons = 50
     let buttonHeight = 35
     let buttonWidth = UIScreen.main.bounds.size.width - 100
     
+    let darkWhite = UIColor(red: 216 / 255, green: 222 / 255, blue: 233 / 255, alpha: 1)
+    let darkestBlue = UIColor(red: 46 / 255, green: 52 / 255, blue: 64 / 255, alpha: 1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        categoriesScrollView.backgroundColor = darkWhite
         
         for index in 0..<self.numberOfButtons {
             let frame1 = CGRect(x: 0, y: 0 + (index * buttonHeight), width: Int(buttonWidth), height: buttonHeight )
             let button = UIButton(frame: frame1)
             button.setTitle("asdasd", for: .normal)
-            button.setTitleColor(UIColor(red: 236 / 255, green: 239 / 255, blue: 244 / 255, alpha: 1), for: .normal)
-            button.backgroundColor = .black
+            button.setTitleColor(darkestBlue, for: .normal)
+//            button.backgroundColor = .black
             button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
             
             self.categoriesScrollView.addSubview(button)
@@ -30,8 +37,14 @@ class SearchViewController: UIViewController {
         self.categoriesScrollView.contentSize.height = CGFloat(buttonHeight * numberOfButtons)
     }
     
+    @IBAction func categoriesButtonOnClick(_ sender: Any) {
+        UIView.animate(withDuration: 0.35) {
+            self.categoriesScrollView.isHidden = !self.categoriesScrollView.isHidden
+        }
+    }
+    
     @objc func buttonAction(sender: UIButton!) {
-        print("button: \(sender.titleLabel?.text)")
+//        print("button: \(sender.titleLabel?.text)")
     }
     
     override func viewDidAppear(_ animated: Bool) {
