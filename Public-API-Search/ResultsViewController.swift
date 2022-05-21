6cbfe0616284521e190166302f319605d1f8f1b0
Liam Tran <liamtran@Liams-MacBook-Air.local>
2022-05-21 22:47:01 +1000

    Applied stashed changes.

--- Public-API-Search/ResultsViewController.swift
+++ Public-API-Search/ResultsViewController.swift
@@ -30,20 +30,87 @@ class ResultsViewController: UIViewController, UITableViewDelegate, UITableViewD
     }()
 
     private var publicApisList = [publicApis]()
-    lazy var searchBar:UISearchBar = UISearchBar()
+    
+    let searchBar = UISearchBar();
     
     override func viewDidLoad() {
         super.viewDidLoad();
-        publicApisList = [
-            publicApis(title: "Title: Liam", details: ["Auth:", "Category:", "Link:"].compactMap({ return "\($0)" })),
-            publicApis(title: "Christian", details: ["Auth: ", "Category: ", "Link: "].compactMap({ return "\($0)" }))
-        ]
-                
+        
+        publicApisList = []
+
+        for entry in entries {
+            let title = entry.API
+            let auth = entry.Auth.isEmpty ? "Auth: None" : "Auth: \(entry.Auth)"
+            let category = "Category: \(entry.Category)"
+            let description = "Description: \(entry.Description)"
+            let link = "Link: \(entry.Link)"
+            
+            let details = [category, auth, description, link]
+            
+            publicApisList.append(publicApis(title: title, details: details))
+        }
+        
+        configureSearchBarUI()
+        configureTableViewUI()
+    }
+    
+    func configureSearchBarUI() {
+        view.backgroundColor = .white
+        searchBar.sizeToFit()
+        searchBar.delegate = self
+        navigationController?.navigationBar.barTintColor = UIColor(red: 55/255, green: 120/255, blue: 250/255, alpha: 1)
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.setBackgroundImage(UIImage(named: "results.png"), for: .default)
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.isTranslucent = true
+        navigationItem.title = "Results"
+        showSearchBarButton(shouldShow: true)
+    }
+    
+    func configureTableViewUI(){
         tableView.delegate = self;
         tableView.dataSource = self;
         tableView.frame = view.bounds;
-        tableView.backgroundView = UIImageView(image: UIImage(named: "results.png"))
+        tableView.backgroundView = UIImageView(image: UIImage(named: "results.png"));
         view.addSubview(tableView);
+    }
+    
+    @objc func handleShowSearchBar() {
+        searchBar.becomeFirstResponder()
+        search(shouldShow: true)
+    }
+    
+    func showSearchBarButton(shouldShow: Bool) {
+        if shouldShow {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search,
+                                                                target: self,
+                                                                action: #selector(handleShowSearchBar))
+        } else {
+            navigationItem.rightBarButtonItem = nil
+        }
+    }
+    
+    func search(shouldShow: Bool) {
+        showSearchBarButton(shouldShow: !shouldShow)
+        searchBar.showsCancelButton = shouldShow
+        navigationItem.titleView = shouldShow ? searchBar : nil
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        print("Search bar editing did begin..")
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        print("Search bar editing did end..")
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        search(shouldShow: false)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("Search text is \(searchText)")
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -69,6 +136,8 @@ class ResultsViewController: UIViewController, UITableViewDelegate, UITableViewD
             cell.textLabel?.text = publicApisList[indexPath.section].details[indexPath.row - 1]
             cell.backgroundColor = UIColor(white: 1, alpha: 1);
             cell.textLabel?.textColor = UIColor(red: 0.30, green: 0.34, blue: 0.42, alpha: 1.00)
+            cell.textLabel!.numberOfLines = 0
+            cell.textLabel!.lineBreakMode = .byWordWrapping
         }
         return cell;
     }
