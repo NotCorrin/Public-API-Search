fab73b2afebb46f3fb0f973b3c0c677adc2d1919
NotCorrin <43767802+NotCorrin@users.noreply.github.com>
2022-05-22 00:18:22 +1000

    Removed semi-colons

--- Public-API-Search/ResultsViewController.swift
+++ Public-API-Search/ResultsViewController.swift
@@ -15,9 +15,9 @@ class publicApis {
     init(title: String,
          details: [String],
          isOpened: Bool = false) {
-        self.title = title;
-        self.details = details;
-        self.isOpened = isOpened;
+        self.title = title
+        self.details = details
+        self.isOpened = isOpened
     }
 }
 
@@ -34,10 +34,10 @@ class ResultsViewController: UIViewController, UITableViewDelegate, UITableViewD
 
     private var publicApisList = [publicApis]()
     
-    let searchBar = UISearchBar();
+    let searchBar = UISearchBar()
     
     override func viewDidLoad() {
-        super.viewDidLoad();
+        super.viewDidLoad()
 
         publicApisList = []
         
@@ -56,45 +56,45 @@ class ResultsViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
     
     func configureTableViewUI(){
-        tableView.delegate = self;
-        tableView.dataSource = self;
-        tableView.frame = view.bounds;
-        tableView.backgroundView = UIImageView(image: UIImage(named: "results.png"));
-        clippedView.addSubview(tableView);
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+        tableView.backgroundView = UIImageView(image: UIImage(named: "results.png"))
+        clippedView.addSubview(tableView)
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return publicApisList.count;
+        return publicApisList.count
     }
     
     func tableView( _ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let section = publicApisList[section]
         if section.isOpened {
-            return section.details.count + 1;
+            return section.details.count + 1
         } else {
-            return 1;
+            return 1
         }
     }
     
     func tableView( _ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath);
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         if indexPath.row == 0 {
-            cell.textLabel?.text = publicApisList[indexPath.section].title;
-            cell.backgroundColor = UIColor(red: 0.30, green: 0.34, blue: 0.42, alpha: 1.00);
-            cell.textLabel?.textColor = UIColor(white: 1, alpha: 1);
+            cell.textLabel?.text = publicApisList[indexPath.section].title
+            cell.backgroundColor = UIColor(red: 0.30, green: 0.34, blue: 0.42, alpha: 1.00)
+            cell.textLabel?.textColor = UIColor(white: 1, alpha: 1)
         } else {
             cell.textLabel?.text = publicApisList[indexPath.section].details[indexPath.row - 1]
-            cell.backgroundColor = UIColor(white: 1, alpha: 1);
+            cell.backgroundColor = UIColor(white: 1, alpha: 1)
             cell.textLabel?.textColor = UIColor(red: 0.30, green: 0.34, blue: 0.42, alpha: 1.00)
             cell.textLabel!.numberOfLines = 0
             cell.textLabel!.lineBreakMode = .byWordWrapping
         }
-        return cell;
+        return cell
     }
     
     func tableView( _ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true);
-        publicApisList[indexPath.section].isOpened = !publicApisList[indexPath.section].isOpened;
-        tableView.reloadSections([indexPath.section], with: .none);
+        tableView.deselectRow(at: indexPath, animated: true)
+        publicApisList[indexPath.section].isOpened = !publicApisList[indexPath.section].isOpened
+        tableView.reloadSections([indexPath.section], with: .none)
     }
 }
